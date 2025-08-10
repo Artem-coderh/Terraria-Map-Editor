fde4ee4ee61e2b44b8fb69e7ff46ba0e9acbc34f
Artem <115065432+Artem-coderh@users.noreply.github.com>
2025-08-10 11:27:51 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -18,6 +18,7 @@ if (Test-Path -Path ".\$ReleasePath") { Remove-Item -Path ".\$ReleasePath" -Forc
 New-Item -Path ".\$ReleasePath\" -Force -ItemType "directory"
 
 .\build-legacy.ps1 -ReleasePath $ReleasePath -VersionPrefix $VersionPrefix -VersionSuffix $VersionSuffix
-# .\build-avalonia.ps1 -ReleasePath $ReleasePath -VersionPrefix $VersionPrefix -VersionSuffix $VersionSuffix
+.\build-avalonia.ps1 -ReleasePath $ReleasePath -VersionPrefix $VersionPrefix -VersionSuffix $VersionSuffix
+
 
 
