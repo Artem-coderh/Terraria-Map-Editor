76a0bf360b9016f870970b8ad1d00e481ddd615b
Artem <115065432+Artem-coderh@users.noreply.github.com>
2025-08-10 11:19:16 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -17,6 +17,7 @@ if ($versionSplit.Length -gt 1) {
 if (Test-Path -Path ".\$ReleasePath") { Remove-Item -Path ".\$ReleasePath" -Force -Recurse }
 New-Item -Path ".\$ReleasePath\" -Force -ItemType "directory"
 
-# .\build-legacy.ps1 -ReleasePath $ReleasePath -VersionPrefix $VersionPrefix -VersionSuffix $VersionSuffix
-.\build-avalonia.ps1 -ReleasePath $ReleasePath -VersionPrefix $VersionPrefix -VersionSuffix $VersionSuffix
+.\build-legacy.ps1 -ReleasePath $ReleasePath -VersionPrefix $VersionPrefix -VersionSuffix $VersionSuffix
+# .\build-avalonia.ps1 -ReleasePath $ReleasePath -VersionPrefix $VersionPrefix -VersionSuffix $VersionSuffix
+
 
